7dd331ab96cf8fbe598322b3a13e9d80a3a40585
jozef kubis <kubis.jozef@outlook.com>
2024-08-15 17:36:14 +0200

    Upraveny Footer.jsx a Footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
-function Footer() {
-  return <footer>Footer</footer>
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Copyright © 2024 {"Jozef Kubis"}</p>
+      <p>All rights reserved</p>
+      <p>Contact: {"kubis.jozef@outlook.com"}</p>
+      <p>Design: {"Jozef Kubis"}</p>
+    </footer>
+  )
 }
 
 export default Footer
